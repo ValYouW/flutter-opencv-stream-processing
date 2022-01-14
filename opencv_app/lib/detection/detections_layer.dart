340d63d13cc6899c0be719ce1f7cb9089fb86862
ValYouW <ValYouW@gmail.com>
2022-01-14 18:02:10 +0200

    Create the detection page and a detector stub

---
+++ opencv_app/lib/detection/detections_layer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetectionsLayer extends StatefulWidget {
+  const DetectionsLayer({Key? key}) : super(key: key);
+
+  @override
+  _DetectionsLayerState createState() => _DetectionsLayerState();
+}
+
+class _DetectionsLayerState extends State<DetectionsLayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ opencv_app/lib/detector/aruco_detector.dart
@@ -0,0 +1,11 @@
+import 'dart:typed_data';
+
+import 'package:camera/camera.dart';
+
+class ArucoDetector {
+  Float32List? detect(CameraImage image, int rotation) {
+    return null;
+  }
+
+  destroy() {}
+}
